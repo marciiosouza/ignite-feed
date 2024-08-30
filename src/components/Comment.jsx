2ec784f60ba6c3d8598c081095c61d44d677ca8b
Marcio Souza <marciiosouza@outlook.com>
2024-08-30 13:08:20 -0300

    Comment stylizations

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/marciiosouza.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Marcio Souza</strong>
+              <time title="29 de Agosto de 2024" dateTime="2024-08-29 17:28:00">
+                Publicado há 1h
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
